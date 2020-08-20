4d9f201a527ddf3f7560b5588d6889190239ebc5
iiZhangJun <844215862@qq.com>
2020-08-20 19:15:51 +0800

    LeetCode每日积累

---
+++ src/com/tanxin/Leetcode1326.java
@@ -0,0 +1,18 @@
+package com.tanxin;
+
+/**
+ * 1326. 灌溉花园的最少水龙头数目
+ * ranges 代表当前位置的水龙头，向左向右可以覆盖多少块地
+ * 定义 一个 land 数据
+ *     代表在所有能够覆盖这块土地的所有水龙头中，找到能够覆盖最远（右边）位置的水龙头，记录它最右覆盖的土地
+ */
+public class Leetcode1326 {
+
+    public int minTaps(int n, int[] ranges) {
+
+    }
+
+
+
+
+}
